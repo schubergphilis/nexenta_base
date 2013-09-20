0deaef40db8061e1d08518f0973664f68428b0a5
Brenn Oosterbaan - SBP -SE <brenn@mccmadm1.(none)>
2013-09-20 15:48:53 +0200

    prettier

--- attributes/default.rb
+++ attributes/default.rb
@@ -21,10 +21,10 @@
 default["nexenta"]["nms"]["ses_check_flapping_default"]     = "2" # Default = 0. Must be between 0 and 9.
 default["nexenta"]["nms"]["nms_reporter_default"]           = "disable" # Default = enable. Must be either 'enable' or 'disable'.
 
-# /etc/logadm/conf                                                                                                                             # Add log rotation for nmv.log.
+# /etc/logadm/conf                                          # Add log rotation for nmv.log.
 default["nexenta"]["logadm"]["nmv_log_rotate_default"]      = "10m" # must be in format 'XXm'.
 
-# /etc/default/nfs                                                                                                                               # NFS settings.
+# /etc/default/nfs                                          # NFS settings.
 default["nexenta"]["nfs"]["nfsd_listen_backlog"]            = "64" # Default
 default["nexenta"]["nfs"]["nfsd_protocol"]                  = "ALL" # Default
 default["nexenta"]["nfs"]["nfsd_servers"]                   = "1024" # Default
@@ -35,10 +35,10 @@ default["nexenta"]["nfs"]["grace_period"]                   = "90" # Default
 default["nexenta"]["nfs"]["nfs_server_versmax"]             = "3" # Default = 4
 default["nexenta"]["nfs"]["nfs_client_versmax"]             = "3" # Default = 4
 
-# /etc/inet/ntp.conf                                                                                                                            # NTP server.
+# /etc/inet/ntp.conf                                        # NTP server.
 default["nexenta"]["ntp"]["timeserver"]                     = "ntp.pool.org"
 
-# /etc/snmp/snmpd.conf                                                                                                                     # SNMP settings.
+# /etc/snmp/snmpd.conf                                      # SNMP settings.
 default["nexenta"]["snmp"]["rocommunity"]                   = "public"
 default["nexenta"]["snmp"]["sysDescr"]                      = "NexentaOS"
 default["nexenta"]["snmp"]["sysLocation"]                   = ""
@@ -47,14 +47,14 @@ default["nexenta"]["snmp"]["trapsink"]                      = "localhost"
 default["nexenta"]["snmp"]["linkUpDownNotifications"]       = "yes"
 default["nexenta"]["snmp"]["master"]                        = "agentx"
 
-# /etc/syslog.conf                                                                                                                               # Loghosts.
+# /etc/syslog.conf                                          # Loghosts.
 default["nexenta"]["syslog"]["loghosts"]                    =["loghost1", "loghost2"] # Any number of hosts can be configured.
 
-# /etc/resolv.conf                                                                                                                               # Name resolution settings.
-default["nexenta"]["resolv"]["search"]                      = "mgt.yourcompany.net prd.yourcompany.net" # Only extra search domains. The default domain is already added. Any number of search domains can be configured.
+# /etc/resolv.conf                                          # Name resolution settings.
+default["nexenta"]["resolv"]["search"]                      = "mgt.yourcompany.net prd.yourcompany.net" # Only extra search domains, space seperated. The default domain is already added.
 default["nexenta"]["resolv"]["nameservers"]                 = ["10.2.3.4", "10.2.3.5"] # Any number of nameservers can be configured.
 
-# /etc/system                                                                                                                                       # ZFS tunables. In effect after reboot.
+# /etc/system                                               # ZFS tunables. In effect after reboot.
 default["nexenta"]["system"]["zfs_resilver_delay"]          = "2" # Default
 default["nexenta"]["system"]["zfs_txg_synctime_ms"]         = "5000" #Default
 default["nexenta"]["system"]["zfs_txg_timeout"]             = "10" # Default
@@ -63,6 +63,6 @@ default["nexenta"]["system"]["l2arc_write_boost"]           = "83886080" # Non-D
 default["nexenta"]["system"]["nfs3_max_transfer_size"]      = "131072" # Non-Default. Changed to benefit our 10GB hypervisor environment. Leave empty to use defaults.
 default["nexenta"]["system"]["nfs3_max_transfer_size_cots"] = "131072" # Non-Default. Changed to benefit our 10GB hypervisor environment. Leave empty to use defaults.
 
-# /root/.ssh/authorized_keys                                                                                                            # Public SSH keys. Partner key is also added. Any number of attributes with a SSH key can be added.
+# /root/.ssh/authorized_keys                                # Public SSH keys. Partner key is also added. Any number of attributes with a SSH key can be added.
 default["nexenta"]["authorized_keys"]["joe"]                = "ssh-dss AAAAB3NzaC1kc3MAAACBAI0abRL2d3tA9Zt81RZfFlGR2i/FQFi8PFgYwmyHcpxqYycDyP9X69DCDUvItViKIWApxrOCwb49sPFQC7i6ZAtIrOItsILW7i2iY+NjlVJQAAAIAUkqFKuqE1K6ffXLQUEud1x6KsfDUKPH7Li4gCFomuPZEF8xykNKa4YMfj9C+ZfLkpqme3548qaWJx6vTIpLh+DmE41bnk/RCYKY54pZi/NUIKe0EsHYQHetIpZRoVEDMscZXrbrctdIHEtQaHmJ6pCBtRILYT6gGFH+v9OfX7GgAAAIBqb0Y1jibPE74L1Q7g+/y/0aLmMwx01R8W8lgghOM5O31MevWeri8eab677xwQq5afZ1RYy+IjPVtaTv9g9vXQ+fS2uFeDILRaWvLW+JjJsxSNG2yuBI7ohuJW+MLTSJkqhC6jFBbrqp+Rny8n/yb4Mufp5ZCm9cjR78YAl1GZ+g== joe"
 default["nexenta"]["authorized_keys"]["jane"]               = "ssh-dss AAAnN3GqO89ZHSPi/1FRY1ajFGaCCDJ7MSohO5CW8FRGSEcLDR2xmOw0K6pqPdIQQXpbHLSAwKP6ydjhTou1HMH5SxKLdOVBjcIe9dJc2O/wdGpHh1zXC2S9ljeUmWrAAAAFQDW0C5kq5SNgCPs1mjBxxZe5wS3DQAAAIEAjqcJex0ijphfVKTh8RnD6n3C8490kHP7t85IOReazD4TqLlgF2jQocspxj2MzgqlK2JGnrHjux+i5lNIFHzKJiIPpx9yHuwsZLcj9ne3BnZbgj6stQYUFaapLgamX8fCkWX3dM8+Iwg/VGtEkpMw5Y2oalW2iwlzgJb/kBmjOxcAAACACk7fbZ5r++CdbkIhRhANhbTx/eUvNVsUEt9O4G7Co4NvbrbXTy+sMH9EAxfK5XAjfbGtyc/YCVLxKoiEwgaIo/f/FDTcOcKGvdFxcGf7fKuMGbqwFwmKAEsnb9kONz77CE7Z6HWwA= jane"
